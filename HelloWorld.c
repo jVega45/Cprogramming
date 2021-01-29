e6d3afefa40c5ea187cb7f797254461fd39602bc
jVega45 <51223457+jVega45@users.noreply.github.com>
2021-01-29 10:51:44 -0800

    Hello World program

---
+++ HelloWorld.c
@@ -0,0 +1,23 @@
+/*
+Date created: 29012021
+This program is a basic Hello World 
+program. I'm still working on be 
+proficient at making commits and 
+pushing the changes made to my GitHub 
+accont.
+Author: Joshua Vega
+*/
+#include <stdio.h>
+
+int main(){
+    
+    printf("Hello World!");
+    int i; // increment variable for loop
+    for(i = 10; i > 0; i--)
+    {
+        printf("%d...\n", i);             
+    }
+    printf("-----Blast Off!-----");
+
+    return 0;
+}
