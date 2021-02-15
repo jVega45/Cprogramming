61658ad87cf2f9903725ba79397feec80c613ac5
jVega45 <joshuaevega772@gmail.com>
2021-02-14 16:51:02 -0800

    memSet & memCpy implementation

---
+++ implement-memSet-memCpy.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * Sets the first n bytes pointed to by str to the value specified by c
+ *
+ * @param   dest - pointer the block of memory to set
+ * @param   c    - value to set; passed as an integer but converted to unsigned
+ * char when set
+ * @param   n    - number of bytes to set
+ * @return  pointer to the memory region being set; NULL on error
+ */
+void *sp_memset(void *dest, int c, size_t n) 
+{
+    unsigned char* ptr = dest; // convert the void ptr to a ptr to an unsigned char
+    while(n > 0)
+    {
+        *ptr = c; // cannot dereference a void pointer, therefore use the ptr variable
+        ptr++;
+        n--;
+    }
+    return dest;
+}
+
+/**
+ * Copies n bytes from the block of memory pointed to by src to dest
+ *
+ * @param   dest - pointer to the destination block of memory
+ * @param   src  - pointer to the source block of memory
+ * @param   n    - number of bytes to read/set
+ * @return  pointer to the destination memory region; NULL on error
+ */
+void *sp_memcpy(void *dest, const void *src, size_t n) 
+{
+    // typecast src and dest addresses to pointers to chars
+    char* cpySrc = (char*)src;
+    char* cpyDest = (char*)dest;
+    int i; // i don't like declaring variables inside header of loops
+
+    for(i = 0; i < n; i++)
+    {
+        cpyDest[i] = cpySrc[i]; // copy src to dest
+    } 
+
+    return dest;
+}
+
+int main (int argc, char* argv[])
+{
+    int arr1[2] = {1, 2};
+    int arr2[2] = {3, 4};
+    sp_memcpy(arr1, arr2, 2 * sizeof(int));
+    printf("\n%08x %08x\n", arr1[0], arr1[1]);
+    sp_memset(arr1, 1, 2 * sizeof(int));
+    printf("\n%08x %08x\n", arr1[0], arr1[1]);
+    return 0;
+}
